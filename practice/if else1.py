98545609349ef8cb8310b53774cd9f936f18f9e1
Rahul <jadhavrahul5237@gmail.com>
2026-03-10 22:18:15 +0530

    added intitial files

---
+++ practice/if else1.py
@@ -0,0 +1,11 @@
+print("program started")
+name =input("enter of your name:")
+age=input("enter of your age:")
+age=int(age)
+if age>=18:
+    print(f"hey {name}, you are eligible for voting")
+
+else:
+    print(f"hey {name}, you are not eligible for voting")
+
+print("program completed")
