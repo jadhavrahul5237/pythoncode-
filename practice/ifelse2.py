cf91f84aecd6d81a1839c95a8c2d593e28860959
Rahul <jadhavrahul5237@gmail.com>
2026-03-10 22:21:10 +0530

    added intitial files

---
+++ practice/ifelse2.py
@@ -0,0 +1,16 @@
+#accepting username and password check authentication
+
+username=input("enter your username:")
+password=input("enter your password:")
+
+if username=='rahul' and password=='rahul@123':
+    print("login ok")
+    print("login ok")
+    print("login ok")
+    print("login ok")
+else:
+    print("login failed")
+    print("login failed")
+    print("login failed")
+
+print("program completed")
